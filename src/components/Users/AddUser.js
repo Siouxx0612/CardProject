68318ac3779fde55297bc70a88c65a9380205a45
Matija <siouxx0612@gmail.com>
2021-08-12 10:42:27 +0200

    userInput handlers

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card'
 import classes from '../Users/AddUser.module.css';
 import Button from '../UI/Button';
@@ -6,17 +6,30 @@ import Button from '../UI/Button';
 
 
 const AddUser = (props) => {
+    //array destructuring as useState Hook has 2 parameters. First parameter is a snapshot, and second parameter is a function that can be called to change that first state which then rerenders.
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge);
+    }
+
+    const userameChangeHander = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     }
 
     return (
         <Card className={classes.input} >
             <form onSubmit={addUserHandler} >
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input id="username" type="text" onChange={userameChangeHander}/>
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" onChange={ageChangeHandler}/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
