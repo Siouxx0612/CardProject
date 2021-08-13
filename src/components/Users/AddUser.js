40bfd5107774fc3a33750f1fb17fee309ceae4da
Matija <siouxx0612@gmail.com>
2021-08-13 16:46:53 +0200

    adding Wrapper instead of <div> SOUP in AddUser

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,7 +3,7 @@ import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
 import classes from '../Users/AddUser.module.css';
 import Button from '../UI/Button';
-
+import Wrapper from '../Helpers/Wrapper';
 
 
 const AddUser = (props) => {
@@ -49,7 +49,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div> 
+        <Wrapper> 
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input} >
                 <form onSubmit={addUserHandler} >
@@ -70,7 +70,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     );
 
 };
